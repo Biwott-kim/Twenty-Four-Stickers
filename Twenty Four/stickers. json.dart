d8b5f3c30fc30773d14e08341e2049109287c775
Biwott-Kim <himits881@gmail.com>
2026-01-29 20:23:46 +0300

    initial commit

---
+++ Twenty Four/stickers. json.dart
@@ -0,0 +1,27 @@
+[
+  {
+    "id": "1",
+    "name": "Cool Anime Sticker",
+    "category": "anime",
+    "img": "C:\Users\Alpaca\Desktop\Stickers\f877dc93-8bdf-4a87-9431-8b6d7c1b271f.jpg"
+  },
+  {
+    "id": "2",
+    "name": "Sporty Fun Sticker",
+    "category": "sporty",
+    "img": "/images/stickers/sporty/sporty1.png"
+  },
+  {
+    "id": "3",
+    "name": "Girly Sparkle Sticker",
+    "category": "girly",
+    "img": "/images/stickers/girly/f877dc93-8bdf-4a87-9431-8b6d7c1b271f.jpg"
+  },
+  {
+    "id": "4",
+    "name": "Gangster Vibe Sticker",
+    "category": "gangster",
+    "img": "/images/stickers/gangster/gangster1.png"
+  }
+]
+
